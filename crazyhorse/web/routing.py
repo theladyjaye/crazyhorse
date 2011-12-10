1b8e84662530f41cc0fa619a06b211d0ff722ccb
Adam Venturella <aventurella@gmail.com>
2011-12-09 22:00:27 -0800

    spacin

--- crazyhorse/web/routing.py
+++ crazyhorse/web/routing.py
@@ -127,6 +127,7 @@ class Route(object):
             pass
         
         method = getattr(obj, action)
+        
         # set the httpcontext for the @authorize 
         # decorator if needed
         method.__dict__["httpcontext"] = context
