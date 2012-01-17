9a8eef9070235dfdab7a3dd8196ce57010aaf1c1
Adam Venturella <aventurella@gmail.com>
2012-01-16 17:38:51 -0800

    added traceback to error or greater in logging

--- crazyhorse/utils/logging/__init__.py
+++ crazyhorse/utils/logging/__init__.py
@@ -1,10 +1,14 @@
 import logging
+import sys
+import traceback
 from crazyhorse.utils.logging import colors
 
 class CrazyHorseStatusFilter(logging.Filter):
 
     def filter(self, record):
         color = None
+        extra = ""
+
         if record.levelno == logging.INFO:
            color = colors.foreground.green
         if record.levelno == logging.DEBUG:
@@ -13,12 +17,15 @@ class CrazyHorseStatusFilter(logging.Filter):
             color = colors.foreground.yellow
         elif record.levelno == logging.ERROR:
             color = colors.foreground.red
+            extra = "\n" + traceback.format_exc(5)
         elif record.levelno == logging.CRITICAL:
             color = colors.foreground.red
+            extra = "\n" + traceback.format_exc(5)
         elif record.levelno == logging.FATAL:
             color = colors.foreground.red
+            extra = "\n" + traceback.format_exc(5)
 
         if color is not None:
-            record.msg = color + "["+record.levelname+"] " + colors.foreground.reset + record.msg
+            record.msg = color + "["+record.levelname+"] " + colors.foreground.reset + record.msg + extra
 
         return True
